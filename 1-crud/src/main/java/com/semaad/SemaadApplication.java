94b4488d36c2cc2dfe6ff52c9f2b0e1ef51d77d4
tavares7 <aquilla.tavares7@gmail.com>
2021-05-05 22:55:28 -0300

    SemaadApplication

---
+++ 1-crud/src/main/java/com/semaad/SemaadApplication.java
@@ -0,0 +1,13 @@
+package com.semaad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemaadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemaadApplication.class, args);
+	}
+
+}
