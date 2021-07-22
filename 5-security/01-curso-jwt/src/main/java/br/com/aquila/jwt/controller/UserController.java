a2e173066df06a5113100949b7db8c2359688770
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-07-21 21:26:01 -0300

    Organização dos pacotes

--- 5-security/01-curso-jwt/src/main/java/br/com/aquila/jwt/controller/UserController.java
+++ 5-security/01-curso-jwt/src/main/java/br/com/aquila/jwt/controller/UserController.java
@@ -3,7 +3,6 @@ package br.com.aquila.jwt.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.aquila.jwt.data.UserData;
