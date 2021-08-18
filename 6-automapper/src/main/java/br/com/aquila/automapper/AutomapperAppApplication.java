f5ca563efd2a270a3bffba99de10e24d42b921e6
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-08-18 15:17:06 -0300

    Criação do projeto

---
+++ 6-automapper/src/main/java/br/com/aquila/automapper/AutomapperAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.aquila.automapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomapperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomapperAppApplication.class, args);
+	}
+
+}
