414ef7d84a335738c6459ee39bdbd5c25ec99037
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-11-20 19:47:05 -0300

    02-curso:03 - Paginação e Cache

---
+++ 01-curso/02-curso/03-aula/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,42 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+}
