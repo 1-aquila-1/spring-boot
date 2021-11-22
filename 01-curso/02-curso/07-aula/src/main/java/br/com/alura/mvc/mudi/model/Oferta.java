1d5f505a813b15133d0c5bd128659184af181bf2
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-11-22 20:56:48 -0300

    02-curso:07 - Monitoramento com interceptadores

---
+++ 01-curso/02-curso/07-aula/src/main/java/br/com/alura/mvc/mudi/model/Oferta.java
@@ -0,0 +1,35 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Oferta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private BigDecimal valor;
+
+	private LocalDate dataDaEntrega;
+
+	private String comentario;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Pedido pedido;
+}
